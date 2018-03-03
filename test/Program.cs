3e27d894db2dd0f54c57c3bb6e9df9c7453852ae
Pavelza <hapass@ya.ru>
2018-03-03 20:19:12 +0300

    More garbage.

--- test/Program.cs
+++ test/Program.cs
@@ -1,28 +1,49 @@
 ﻿using System;
+using System.Media;
 using System.Threading.Tasks;
 
 namespace test
 {
     class MainClass
     {
-        public const int GuessingNumber = 3234;
         public static bool isEnd = false;
         public static bool hasWon = false;
+        public static Random numberRandom;
+
+        public static SoundPlayer sound = new SoundPlayer(Environment.CurrentDirectory + "/print_sound.wav");
 
         public static void Main(string[] args)
         {
+            sound.Load();
+            Console.WriteLine($"Loading complete: {sound.IsLoadCompleted}");
+
             StartGame();
             StartLoop();
         }
 
         public static async void StartGame()
         {
-            await Task.Delay(200);
+            numberRandom = new Random();
+
             await Print("Hi.");
             await Task.Delay(120);
             await Print("Welcome to my game.");
             await Task.Delay(1000);
-            await Print("The rules are simple. You try to guess next number.");
+            await Print("The rules are simple.");
+            await Task.Delay(300);
+            await Print("I give you the list of numbers.");
+            await Task.Delay(600);
+            await Print("You try to guess the next one in a list.");
+            await Task.Delay(800);
+            await Print("Here we go...");
+
+            for (int i = 0; i < 10; i++)
+            {
+                await Task.Delay(900);
+                Console.WriteLine(numberRandom.Next());
+            }
+            await Task.Delay(300);
+            await Print("Now... Your turn:");
         }
 
         public static async Task Print(string message)
@@ -33,6 +54,7 @@ namespace test
             {
                 var timeToFindCharacter = random.Next(10, 100);
                 await Task.Delay(timeToFindCharacter);
+                sound.PlaySync();
                 Console.Write(ch);
             }
 
@@ -72,7 +94,7 @@ namespace test
 
         public static void OnInput(int number)
         {
-            hasWon = number == GuessingNumber;
+            hasWon = number == numberRandom.Next();
             isEnd = true;
         }
     }
